48e6bb465f0b13dc45aa31961c1f3f9df05a2b65
binniethepooh <64795543+binniethepooh@users.noreply.github.com>
2020-07-04 17:06:56 +0900

    #3 Finished

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,7 +25,9 @@ export const postLogin = (req, res) => {
 };
 
 export const logout = (req, res) =>
-  res.render("logout", { pageTitle: "Log Out" });
+  // To Do: Process log out
+  res.redirect(routes.home);
+
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
