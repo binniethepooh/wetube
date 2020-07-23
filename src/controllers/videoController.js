03fe18e57da383bafbccd499f3fc149cf4d311fd
binniethepooh <64795543+binniethepooh@users.noreply.github.com>
2020-07-23 23:47:10 +0900

    Prestart

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -39,10 +39,10 @@ export const getUpload = (req, res) =>
 export const postUpload = async (req, res) => {
   const {
     body: { title, description },
-    file: { path },
+    file: { location },
   } = req;
   const newVideo = await Video.create({
-    fileUrl: path,
+    fileUrl: location,
     title,
     description, // title, description은 이름이 똑같아서 그대로 써도 돼. 원래는 title: title 이런식으로 해야해.
     creator: req.user.id,
